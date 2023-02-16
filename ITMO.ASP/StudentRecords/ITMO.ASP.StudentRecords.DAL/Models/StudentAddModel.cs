b63b9b126b126ffc23e66aa23946bcee6e4399d3
BritvA <apan1998@yandex.ru>
2023-02-16 19:07:36 +0300

    убран лишний атрибут

--- ITMO.ASP/StudentRecords/ITMO.ASP.StudentRecords.DAL/Models/StudentAddModel.cs
+++ ITMO.ASP/StudentRecords/ITMO.ASP.StudentRecords.DAL/Models/StudentAddModel.cs
@@ -12,21 +12,20 @@ namespace ITMO.ASP.StudentRecords.DAL.Models
     public class StudentAddModel
     {
         [StringLength(20, ErrorMessage = "Maximum length - 20")]
-        [MinLength(1, ErrorMessage = "Minimum length - 1")]
+        [MinLength(2, ErrorMessage = "Minimum length - 1")]
         [Required(ErrorMessage = "Fill in the field")]
         [RegularExpression("^[A-Za-z]+$", ErrorMessage = "Use only latin")]
         [DisplayName("First name")]
         public string FirstName { get; set; }
 
         [StringLength(30, ErrorMessage = "Maximum length - 30")]
-        [MinLength(1, ErrorMessage = "Minimum length - 1")]
+        [MinLength(2, ErrorMessage = "Minimum length - 1")]
         [Required(ErrorMessage = "Fill in the field")]
         [RegularExpression("^[A-Za-z]+$", ErrorMessage = "Use only latin")]
         [DisplayName("Last name")]
         public string LastName { get; set; }
 
         [StringLength(1, ErrorMessage = "Maximum length - 1")]
-        [MinLength(1, ErrorMessage = "Minimum length - 1")]
         [Required(ErrorMessage = "Fill in the field")]
         [RegularExpression("^[A-Za-z]+$", ErrorMessage = "Use only latin")]
         [DisplayName("Score")]
