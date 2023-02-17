309ef94b5dcb3bd6b065d9d7852d05b352ba7808
BritvA <apan1998@yandex.ru>
2023-02-17 11:33:49 +0300

    реализована Lab06

---
+++ ITMO.ASP/MVC/Lab06/ITMO.ASP.MVC.Lab06.MvcCreditApp1/Models/Credit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASP.MVC.Lab06.MvcCreditApp1.Models
+{
+    public class Credit
+    {
+        // ID кредита
+        public virtual int CreditId { get; set; }
+        // Название
+        [DisplayName("Название кредита")]
+        [Required]
+        public virtual string Head { get; set; }
+        // Период, на который выдается кредит
+        [DisplayName("Период")]
+        [Required]
+        public virtual int Period { get; set; }
+        // Максимальная сумма кредита
+        [DisplayName("Максимальная сумма")]
+        [Required]
+        public virtual int Sum { get; set; }
+        // Процентная ставка
+        [DisplayName("Процентная ставка")]
+        [Required]
+        public virtual int Procent { get; set; }
+    }
+}
---
+++ ITMO.ASP/MVC/Lab06/ITMO.ASP.MVC.Lab06.MvcCreditApp1/Models/CreditContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ITMO.ASP.MVC.Lab06.MvcCreditApp1.Models
+{
+    public class CreditContext : DbContext
+    {
+        public CreditContext()
+            : base("CreditBD")
+        { }
+        public DbSet<Credit> Credits { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+    }
+}
